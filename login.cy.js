c5f4917d59bd9282fa7d87eeba6b37772a545517
pal-source <pal.nisha17.24@gmail.com>
2024-06-18 22:34:10 +0530

    test

---
+++ login.cy.js
@@ -0,0 +1,8 @@
+describe("To check Login functinality", () => {
+    it("To Verify user will able to login with valid credencial", () => {
+      cy.visit("https://oralb.co.uk/en-gb")
+      
+    })
+  })
+  
+  
