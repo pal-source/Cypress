c736a6db8f613c515514a8d5d3c3ec28e01d63ee
pal-source <108976245+pal-source@users.noreply.github.com>
2024-06-18 22:28:04 +0530

    Add files via upload

---
+++ login.cy.js
@@ -0,0 +1,8 @@
+describe("To check Login functinality", () => {
+    it.only("To Verify user will able to login with valid credencial", () => {
+      cy.visit("https://www.orangehrm.com/en/")
+      
+    })
+  })
+  
+  
